9b1673ec8d82b1fe2e100da0e343b0a9b507c60c
Austin Akers <austin@harperdb.io>
2026-06-22 15:41:22 -0400

    updated method to static methodo

--- resources.js
+++ resources.js
@@ -4,7 +4,7 @@ import { tables } from 'harper';
 
 export class Topic extends tables.Topic {
 	// we can define our own custom subscribe handler for doing things like returning previous messages
-	async subscribe(options) {
+	static async subscribe(options) {
 		if (!options.startTime) // return last five messages
 			options.previousCount = 5;
 		return super.subscribe(options);
